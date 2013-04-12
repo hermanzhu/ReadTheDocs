91a2c2f7e153d3f2cbcdba801aa7894151acb0f3
Antz <antz@cix.co.uk>
2013-04-13 00:54:47 +0200

    Update conf.py

--- conf.py
+++ conf.py
@@ -101,7 +101,7 @@ exclude_patterns = [
                             
                             # The theme to use for HTML and HTML Help pages.  See the documentation for
                             # a list of builtin themes.
-                            html_theme = 'default'
+                            #html_theme = 'default'
                             
                             # Theme options are theme-specific and customize the look and feel of a theme
                             # further.  For a list of options available for each theme, see the
@@ -113,7 +113,7 @@ exclude_patterns = [
                             
                             # The name for this set of Sphinx documents.  If None, it defaults to
                             # "<project> v<release> documentation".
-                            html_title = project
+                            #html_title = project
                             
                             # A shorter title for the navigation bar.  Default is the same as html_title.
                             #html_short_title = None
@@ -130,7 +130,7 @@ exclude_patterns = [
                             # Add any paths that contain custom static files (such as style sheets) here,
                             # relative to this directory. They are copied after the builtin static files,
                             # so a file named "default.css" will overwrite the builtin "default.css".
-                            html_static_path = ['_static']
+                            #html_static_path = ['_static']
                             
                             # If not '', a 'Last updated on:' timestamp is inserted at every page bottom,
                             # using the given strftime format.
@@ -174,7 +174,7 @@ exclude_patterns = [
                             #html_file_suffix = None
                             
                             # Output file base name for HTML help builder.
-                            htmlhelp_basename = 'WritetheDocsdoc'
+                            #htmlhelp_basename = 'WritetheDocsdoc'
                             
                             
                             # -- Options for LaTeX output --------------------------------------------------
