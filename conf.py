243a5c86e7d77b1efea07c2d7a00a2f8e00554a1
Antz <antz@cix.co.uk>
2013-04-13 17:35:28 +0200

    Update conf.py

--- conf.py
+++ conf.py
@@ -40,8 +40,8 @@ source_suffix = '.rst'
 master_doc = 'index'
 
 # General information about the project.
-project = u'Write the Docs'
-copyright = u'2013, Nathan Yergler'
+project = u'MaNGOS Wiki'
+copyright = u'2013, Mangos Foundation'
 
 # The version info for the project you're documenting, acts as replacement for
 # |version| and |release|, also used in various other places throughout the
